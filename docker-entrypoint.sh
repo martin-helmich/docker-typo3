4a2dd4bb16100430995969bd82b3b866746fcc3f
typoworx-de <info@typoworx.de>
2023-03-16 14:14:01 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,8 +3,7 @@ for dir in fileadmin typo3conf typo3temp uploads;
 do
   chown www-data:www-data \${DOCUMENT_ROOT:-/var/www/html}/\${dir}
 done
-
-[ -d /docker-entrypoint.d ] && {
+[ -d /docker-entrypoint.d/ ] && [ $(ls -1 /docker-entrypoint.d/*.sh 2> /dev/null) ] && {
   chmod +x /docker-entrypoint.d/*.sh
   run-parts --report --regex='\.sh' /docker-entrypoint.d
 }
